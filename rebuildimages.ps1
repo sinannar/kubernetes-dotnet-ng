fb1dc1947d02a7ca0ad215ed030f18259735e9aa
Sinan NAR <sinan.nar@gmail.com>
2023-08-20 02:41:49 +1200

    docker commands

--- rebuildimages.ps1
+++ rebuildimages.ps1
@@ -1,8 +1,16 @@
 
 
+docker stop demoappcontainer
+docker stop demoapicontainer
+
+docker rm demoappcontainer
+docker rm demoapicontainer
+
 docker rmi demoapp
 docker rmi demoapi
 
-docker build -t demoapp -f demoapp.dockerfile .
-docker build -t demoapi -f demoapi.dockerfile .
+# docker build -t demoapp -f demoapp.dockerfile .
+# docker build -t demoapi -f demoapi.dockerfile .
 
+# docker run -d -p 30100:80 --name demoapicontainer demoapi
+# docker run -d -p 30200:80 --name demoappcontainer demoapp
